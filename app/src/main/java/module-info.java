ed7b1f149ef509f59db71edf98e402754291baba
Kotori316 <Kotori316@gmail.com>
2021-05-27 22:07:44 +0900

    Added module to access SSL page.

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -2,4 +2,6 @@ module fictional.meme {
     exports com.kotori316.fictional;
     requires java.base;
     requires com.google.gson;
+
+    requires jdk.crypto.ec; // required to access with SSL/TLS
 }
